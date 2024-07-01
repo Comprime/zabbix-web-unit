da10397897674be801e083dc5a656df53552921a
victor <comprime1@yahoo.se>
2024-07-01 14:10:24 +0200

    Add zabbix root scripts to preload

--- src/preload.php
+++ src/preload.php
@@ -1,4 +1,5 @@
 <?php
+
 function preloadDirTree($path) {
     $directory = new RecursiveDirectoryIterator($path);
     $fullTree = new RecursiveIteratorIterator($directory);
@@ -8,6 +9,7 @@ function preloadDirTree($path) {
         }
     }
 }
+
 function preloadDir($path) {
     $directory = new DirectoryIterator($path);
     foreach ($directory as $key => $file) {
@@ -17,9 +19,8 @@ function preloadDir($path) {
     }
 }
 
-error_log('start preloading');
 include_once '/usr/share/zabbix/vendor/autoload.php';
 preloadDirTree('/usr/share/zabbix/conf/');
 preloadDirTree('/usr/share/zabbix/include/');
 preloadDir('/usr/share/zabbix/app/');
-error_log('preloading done');
+preloadDir('/usr/share/zabbix/');
